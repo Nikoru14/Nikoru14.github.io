c13b4ca634c2949726cc6bbe5ecc4eea1b28aaa9
Nikoru14 <nicholosantosbongat@gmail.com>
2022-05-24 23:17:30 +0800

    Update script.js

--- midterm/script.js
+++ midterm/script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  AFRAME.registerComponent("cuphead-event", {
+    AFRAME.registerComponent("cuphead-event", {
     init: function () {
       this.audio = document.querySelector("#footsteps")
       let cuphead = document.querySelector("#cuphead-model")
@@ -9,7 +9,6 @@ window.onload = function () {
         function (e) {
             let cuphead = document.querySelector("#cuphead-model")         
             console.log(cuphead.getAttribute("animation-mixer").clip)
-            
             if(cuphead.getAttribute("animation-mixer").clip === "Cuphead_anim_22"){
                 cuphead.setAttribute("animation-mixer", "clip: Static Pose")   
                 this.audio.pause()
@@ -29,6 +28,8 @@ window.onload = function () {
       this.el.addEventListener(
         "mouseenter",
         function (e) {
+            let text = document.querySelector("#cupheadText")
+            text.setAttribute("visible", true)
           console.log(e)
         }.bind(this)
       )
@@ -36,6 +37,8 @@ window.onload = function () {
       this.el.addEventListener(
         "mouseleave",
         function (e) {
+            let text = document.querySelector("#cupheadText")
+            text.setAttribute("visible", false)
           console.log(e)
         }.bind(this)
       )
@@ -89,5 +92,72 @@ window.onload = function () {
       )
     },
   })
-}
 
+  AFRAME.registerComponent("enemy-event", {
+    init: function () {
+      this.audio = document.querySelector("#boo")
+      let enemy = document.querySelector("#enemy-model")
+      enemy = enemy.setAttribute("animation-mixer", "clip: Static Pose;")
+      this.el.addEventListener(
+        "click",
+        function (e) {
+            let enemy = document.querySelector("#enemy-model")         
+            enemy.setAttribute("animation-mixer", "clip: Take 001; loop: once")
+            this.audio.play()
+          console.log(e)
+        }.bind(this)
+      )
+
+      this.el.addEventListener(
+        "mouseenter",
+        function (e) {
+            let text = document.querySelector("#cupheadText")
+            text.setAttribute("visible", true)
+          console.log(e)
+        }.bind(this)
+      )
+
+      this.el.addEventListener(
+        "mouseleave",
+        function (e) {
+            let text = document.querySelector("#cupheadText")
+            text.setAttribute("visible", false)
+          console.log(e)
+        }.bind(this)
+      )
+
+      this.el.addEventListener(
+        "touchmove",
+        function (e) {
+          //this.audio.play();
+
+          console.log(e)
+        }.bind(this)
+      )
+      this.el.addEventListener(
+        "touchstart",
+        function (e) {
+            console.log(e)
+        }.bind(this)
+      )
+
+      this.el.addEventListener(
+        "touchend",
+        function (e) {
+          console.log(e)
+        }.bind(this)
+      )
+
+      this.el.addEventListener(
+        "animation-finished",
+        function (e) {
+          console.log(e)
+          let enemy = document.querySelector("#enemy-model")
+          enemy.SetAttribute("animation-mixer", "clip: Idle")
+          this.audio.pause()
+          this.audio.currentTime = 0
+        }.bind(this)
+      )
+    },
+  })
+}
